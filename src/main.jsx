9d24763b2c1c419b38b172836862f849261aa32f
IvashkaVIP <ivashka.co@gmail.com>
2024-02-03 21:02:24 +0200

    add redux & persistor

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store } from '../src/redux/store.js';
+import { persistor } from '../src/redux/store.js';
 import App from './App.jsx';
 import './index.css';
 import { GlobalStyles } from './GlobalStyles.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/CMU">
-      <GlobalStyles  />
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/CMU">
+          <GlobalStyles />
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
