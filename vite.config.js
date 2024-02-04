03cf013eecc027930ed73bb31f37ccbc89752748
IvashkaVIP <ivashka.co@gmail.com>
2024-02-04 21:11:00 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
-        },
-      },
-      base: '/CMU/',
-    });
+  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      external: ['redux-persist/integration/react'],
+    },
+  },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+    },
+  },
+  base: '/CMU/',
+});
